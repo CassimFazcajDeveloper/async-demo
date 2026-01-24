65586920b3c37c61b9238a13761870f209a061f9
CassimFazcajDeveloper <cassimmikochi@gmail.com>
2026-01-24 10:02:19 +0200

    Successsifully undesctood the concept of async and callback function

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-getUser(1, (user) => {
-    getRepositories(user.username,(repos ) =>{
-        console.log('repos' );
-    })
+console.log('before');
+getUser(1, function(user){
+    console.log('user', user)
+
+    getrepositories(user.githubRepo, (repos)=> console.log('repos', repos))
 });
+console.log('after');
 
 
-function getRepositories (username, Callback){
-    setTimeout(() => {
-        console.log('Checking from the databse...')
-    }, 2000);
-    Callback([repo1, repo2, repo3, repo4]);
-}
+function getUser(id, callback) {
+
+    setTimeout(() =>{
+        console.log('loading the timer...');
+        callback({id: id, githubRepo : 'cassim'});
+    }, 2000);   
+
+
+};
+
+function getrepositories(username, callback){
+    setTimeout(
+        ()=>{
+            console.log('getting github repo API..' );
+            callback(['repo1', 'repo2', 'repo3', 'repo4']); 
+        }
+    ,2000);
+};
+
+
